fb313c5e466157cc9aa63d2520b249202b51fd5b
wassimmastour <wassim.mastour@outlook.com>
2024-01-16 13:29:43 +0100

    fixing vercel issues

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -1,6 +1,11 @@
+// lib/stripe.ts
+
 import Stripe from "stripe";
 
+const latestSupportedVersion = "2023-10-16"; // Replace with the actual latest version
+
+// @ts-ignore
 export const stripe = new Stripe(process.env.STRIPE_API_KEY!, {
-  apiVersion: "2023-10-16",
+  apiVersion: latestSupportedVersion,
   typescript: true,
 });
